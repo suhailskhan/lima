990a4f74c06d6376f9e68c5f66ec322d884b664a
Balaji Vijayakumar <kuttibalaji.v6@gmail.com>
2024-06-24 19:53:17 +0530

    Revamp port forwarding to support UDP
    Signed-off-by: Balaji Vijayakumar <kuttibalaji.v6@gmail.com>

--- pkg/guestagent/api/client/client.go
+++ pkg/guestagent/api/client/client.go
@@ -2,10 +2,12 @@ package client
 
 import (
 	"context"
+	"math"
 	"net"
 
 	"github.com/lima-vm/lima/pkg/guestagent/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/resolver"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -15,13 +17,18 @@ type GuestAgentClient struct {
 
 func NewGuestAgentClient(dialFn func(ctx context.Context) (net.Conn, error)) (*GuestAgentClient, error) {
 	opts := []grpc.DialOption{
-		grpc.WithContextDialer(func(ctx context.Context, target string) (net.Conn, error) {
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(math.MaxInt64),
+			grpc.MaxCallSendMsgSize(math.MaxInt64),
+		),
+		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
 			return dialFn(ctx)
 		}),
 		grpc.WithTransportCredentials(NewCredentials()),
 	}
 
-	clientConn, err := grpc.Dial("", opts...)
+	resolver.SetDefaultScheme("passthrough")
+	clientConn, err := grpc.NewClient("", opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -56,4 +63,12 @@ func (c *GuestAgentClient) Inotify(ctx context.Context) (api.GuestService_PostIn
 		return nil, err
 	}
 	return inotify, nil
+}
+
+func (c *GuestAgentClient) Tunnel(ctx context.Context) (api.GuestService_TunnelClient, error) {
+	stream, err := c.cli.Tunnel(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return stream, nil
 }
